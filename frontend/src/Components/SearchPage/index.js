e4f02e511f07b80c003e0c2ffb8a013e9fc3d71b
= <dominic.freeman123@gmail.com>
2019-05-02 10:03:57 +0100

    Modal beginnings

--- frontend/src/Components/SearchPage/index.js
+++ frontend/src/Components/SearchPage/index.js
@@ -1,20 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import VenueSearchItem from '../VenueSearchItem'
+import './SearchPage.scss'
 
 export default function SearchPage () {
 
     const [venues, setVenues] = useState([]);
+    const [locationModalVisible, setVisibleStatus] = useState(false);
+
     useEffect(() => {
       if(venues.length > 0) return;
 
       axios.get('api.json')
         .then(function (response) {
           setVenues(response.data)
-          
+
         })
 
-    }) 
+    })
 
     console.log(venues);
 
@@ -22,7 +25,11 @@ export default function SearchPage () {
             <main className="govuk-main-wrapper">
                 <div className="govuk-grid-row">
                     <div className="govuk-grid-column-two-thirds">
-                      <strong class="govuk-tag">Location</strong>&nbsp;&nbsp;
+                      <strong class="govuk-tag" onClick={() => setVisibleStatus(true)}>Location</strong>&nbsp;&nbsp;
+                      <div className="location-modal" style={{display: locationModalVisible ? 'inline-block' : 'none'}}>
+                        <a href="#" class="govuk-back-link" onClick={(e) => {e.preventDefault(); setVisibleStatus(false)}}>Close</a>
+                        This is the modal
+                      </div>
                       <strong class="govuk-tag">Size</strong>&nbsp;&nbsp;
                       <strong class="govuk-tag">Date</strong>
                       <br /><br />
