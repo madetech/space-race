c2a75d6fba6984511642964a86796fd9865ce889
Craig J. Bass <craig@madetech.co.uk>
2019-05-02 13:09:59 +0100

    Add map to venue detail

--- frontend/src/Components/VenuePage/index.js
+++ frontend/src/Components/VenuePage/index.js
@@ -3,31 +3,57 @@ import GridColumn from '../GridColumn'
 import GridRow from '../GridRow'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
+import Map from 'pigeon-maps'
+import Marker from 'pigeon-marker'
+
 
 export default function VenuePage (props) {
     const [venue, setVenue] = useState(null)
+    const [latlong, setLatLong] = useState(null)
 
     useEffect(() => {
         if (venue) return
 
         axios.get('/api.json')
-            .then(function (response) {
-                setVenue(response.data.filter(venue => {
+            .then(response => {
+                let venue = response.data.filter(venue => {
                     return venue.HACK_UUID == props.id
-                })[0])
+                })[0]
+                axios.get(`https://api.postcodes.io/postcodes/${venue.POST_CODE}`)
+                    .then(response => {
+                        let result = response.data.result
+                        let numbers = [result.latitude, result.longitude]
+                        setLatLong(numbers)
+                    })
+                setVenue(venue)
             })
     })
 
-    if(!venue) return <></>
+    if (!venue) return <></>
+    const position = [51.505, -0.09]
 
     return <>
         <GridRow>
             <GridColumn>
+                <Link to={'/'} className="govuk-back-link">Back</Link>
                 <h1>{venue.SITE_NAME}</h1>
-                <Link to={"/book/"+props.id} className="govuk-button govuk-button--start" role="button" draggable="false">
+                <Link to={'/book/' + props.id} className="govuk-button govuk-button--start"
+                      role="button" draggable="false">
                     Book now
                 </Link>
+
             </GridColumn>
         </GridRow>
+        <div>
+            {latlong ? <Map center={latlong} zoom={16} height={400}>
+                <Marker anchor={latlong} payload={1} />
+            </Map> : <></>}
+        </div>
+        <GridRow>
+            <GridColumn>
+                more
+            </GridColumn>
+        </GridRow>
+
     </>
 }
