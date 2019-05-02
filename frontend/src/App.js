a5ada81a49ef37770d6e1caffe554e003581ca35
MadeTech Dushan <dushan@madetech.com>
2019-05-02 10:05:12 +0100

    Add back button component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,28 @@
 import React from 'react'
 import Footer from './Components/Footer'
-import SearchPage from './Components/SearchPage'
 import './App.scss'
 import VenueSearchItem from './Components/VenueSearchItem'
+import BookingForm from './Components/BookingForm'
 
 function App () {
-
     return <>
-        <SearchPage />
+        <div className="govuk-width-container">
+            <main className="govuk-main-wrapper">
+                <div className="govuk-grid-row">
+                    <div className="govuk-grid-column-two-thirds">
+                        <a href="#">Location</a><a href="#">Size</a><a href="#"></a>
+                    </div>
+                    <div className="govuk-grid-column-two-thirds venue-search-items">
+                        <VenueSearchItem />
+                        <VenueSearchItem />
+                        <VenueSearchItem />
+                    </div>
+                </div>
+            </main>
+        </div>
         <Footer />
     </>
+
 }
 
 export default App
