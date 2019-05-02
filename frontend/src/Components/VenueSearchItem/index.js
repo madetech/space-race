5c602ac83b604c33dc2fa037e5750407f4e6b19e
= <dominic.freeman123@gmail.com>
2019-05-02 14:05:34 +0100

    Make images full with on index page

--- frontend/src/Components/VenueSearchItem/index.js
+++ frontend/src/Components/VenueSearchItem/index.js
@@ -4,13 +4,13 @@ import image from './venue1.jpeg'
 
 export default function VenueSearchItem (props) {
     return <div className="venue-search-item" onClick={() => props.onClick(props.id)}>
+        <div className="venue-image-container">
+          <div className="venue-image" style={{ "background-image": "url(/venue_images/" + props.id + "/1.jpg)"}}></div>
+        </div>
         <div className="venue-title">
           <h3>
             {props.siteName}
           </h3>
-        </div>
-        <div classNAme="venue-image">
-          <img className="featured" src={"/venue_images/" + props.id + "/1.jpg"} />
         </div>
     </div>
 }
