6f7dea5e2a9d402db7ae9d8eaf45f3efbe344951
Craig J. Bass <craig@madetech.co.uk>
2019-05-02 14:15:21 +0100

    Exit without close button

--- frontend/src/Components/Modal/index.js
+++ frontend/src/Components/Modal/index.js
@@ -4,12 +4,16 @@ import GridColumn from '../GridColumn'
 
 export default function Modal (props) {
 
-  return <div className="location-modal" style={{display: props.visible ? 'inline-block' : 'none'}}>
-    <GridRow>
-      <GridColumn>
-        <a href="#" class="govuk-back-link" onClick={props.onClose}>Close</a>
-        {props.children}
-      </GridColumn>
-     </GridRow>
-  </div>
+    return <>
+        <div className="location-modal"
+             style={{ display: props.visible ? 'inline-block' : 'none' }}>
+            <GridRow>
+                <GridColumn>
+                    <a href="#" class="govuk-back-link" onClick={props.onClose}>Close</a>
+                    {props.children}
+                </GridColumn>
+            </GridRow>
+        </div>
+        <div onClick={props.onClose} style={{ display: props.visible ? 'inline-block' : 'none' }} className="modal-close-overlay"></div>
+    </>
 }
