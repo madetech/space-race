a03bddaa5f5cea9bc2099a785d805b650883e7fe
MadeTech Dushan <dushan@madetech.com>
2019-05-02 12:55:22 +0100

    fix form and use gov uk styling

--- frontend/src/Components/BookingForm/index.js
+++ frontend/src/Components/BookingForm/index.js
@@ -4,13 +4,34 @@ import GridRow from '../GridRow';
 
 export default function BookingForm () {
     return <GridRow>
-    <form className="BookingForm">
-      <input type="text" name="name" value="" className="BookingFormField" />
+    <form className="BookingForm govuk-form-group">
+      <label class="govuk-label" for="fullname">
+      Full Name
+      </label>
+      <span id="name-hint" class="govuk-hint">
+      We’ll only use this to send you a booking confirmation
+      </span>
+      <input type="text" name="fullname" className="govuk-input" />
       <br />
-      <input type="email" name="email" className="BookingFormField" />
       <br />
-      <input type="tel" name="phonenumber" className="BookingFormField" />
-      <input type="submit" value="Send Request" />
+      <label class="govuk-label" for="email">
+      Email Address
+      </label>
+      <span id="email-hint" class="govuk-hint">
+      We’ll only use this to send you a booking confirmation email
+      </span>
+      <input type="email" name="email" className="govuk-input" />
+      <br />
+      <label class="govuk-label" for="phonenumber">
+      Phone Number
+      </label>
+      <span id="phone-hint" class="govuk-hint">
+      We need this to confirm your booking with you
+      </span>
+      <input type="tel" name="phonenumber" className="govuk-input" />
+      <br />
+      <br />
+      <input type="submit" value="Send Request" className="govuk-button" />
     </form>
     </GridRow>
 }
