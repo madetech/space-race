f7f69528a518abe7cf0748f31b6cf22c151b554f
= <dominic.freeman123@gmail.com>
2019-05-02 11:30:06 +0100

    Add some styles and logo

--- frontend/src/Components/VenueSearchItem/index.js
+++ frontend/src/Components/VenueSearchItem/index.js
@@ -4,7 +4,11 @@ import image from './venue1.jpeg'
 
 export default function VenueSearchItem (props) {
     return <div className="venue-search-item">
-        {props.siteName}
+        <div className="venue-title">
+          <h3>
+            {props.siteName}
+          </h3>
+        </div>
         <img className="featured" src={image} />
     </div>
 }
